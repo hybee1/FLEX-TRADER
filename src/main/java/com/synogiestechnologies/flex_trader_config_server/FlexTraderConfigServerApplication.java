b55bcaa00e9132316258c5f91ad74018c2d481d7
hybee1 <anibaba.ibraheem@gmail.com>
2025-08-21 02:29:23 +0100

    added config-server service

---
+++ src/main/java/com/synogiestechnologies/flex_trader_config_server/FlexTraderConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.synogiestechnologies.flex_trader_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class FlexTraderConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexTraderConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/synogiestechnologies/flex_trader_config_server/FlexTraderConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synogiestechnologies.flex_trader_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexTraderConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
